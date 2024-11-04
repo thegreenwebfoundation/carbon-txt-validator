4aaba0e00eb663d083d3a145db6fd5f97118cc94
Chris Adams <chris@productscience.co.uk>
2024-11-05 00:51:54 +0100

    Add wildcard subdomains for carbon-txt serve

--- src/carbon_txt/web/config/settings/production.py
+++ src/carbon_txt/web/config/settings/production.py
@@ -1,7 +1,7 @@
 from .base import *  # noqa
 
 DEBUG = False
-ALLOWED_HOSTS = ["127.0.0.1", "localhost"]
+ALLOWED_HOSTS = ["127.0.0.1", "localhost", ".localhost", ".greenweb.org"]
 
 WSGI_APPLICATION = "carbon_txt.web.config.wsgi.application"
 ROOT_URLCONF = "carbon_txt.web.config.urls"
