5a21ce2a7fb809ba836469f94e7b4f446784dfbc
Chris Adams <chris@productscience.co.uk>
2025-01-02 12:17:09 +0100

    Add a rudimentary checker with some readable output

--- docs/csrd_checker.py
+++ docs/csrd_checker.py
@@ -81,20 +81,61 @@ def __(parsed_report_data):
         if parsed_report_data:
             return parsed_report_data.esrs_datapoints[0]
 
-    fetch_datapoints()
-    return (fetch_datapoints,)
+    vals = fetch_datapoints()
+    return fetch_datapoints, vals
 
 
 @app.cell
 def __(parsed_report_data):
     def show_datapoint_values():
         if parsed_report_data:
-            return parsed_report_data.get_esrs_datapoint_values(
-                parsed_report_data.esrs_datapoints[0]
+            vals = parsed_report_data.get_esrs_datapoint_values(
+                [parsed_report_data.esrs_datapoints[0]]
             )
+            return vals
 
-    show_datapoint_values()
-    return (show_datapoint_values,)
+    val = show_datapoint_values()
+    val
+    return show_datapoint_values, val
+
+
+@app.cell
+def __(mo, val):
+    from carbon_txt.processors import DataPoint, NoMatchingDatapointsError
+
+    message = None
+
+    if isinstance(
+        val["PercentageOfRenewableSourcesInTotalEnergyConsumption"][0], DataPoint
+    ):
+        message = f"""
+        {val["PercentageOfRenewableSourcesInTotalEnergyConsumption"][0].name} was  {val["PercentageOfRenewableSourcesInTotalEnergyConsumption"][0].value:.2%} between {val["PercentageOfRenewableSourcesInTotalEnergyConsumption"][0].start_date} and {val["PercentageOfRenewableSourcesInTotalEnergyConsumption"][0].end_date}.
+        """
+
+    mo.md(message).callout(kind="success") if message else None
+    return DataPoint, NoMatchingDatapointsError, message
+
+
+@app.cell
+def __(NoMatchingDatapointsError, mo, val):
+    error_message = None
+    if isinstance(
+        val["PercentageOfRenewableSourcesInTotalEnergyConsumption"][0],
+        NoMatchingDatapointsError,
+    ):
+        parse_errors = val["PercentageOfRenewableSourcesInTotalEnergyConsumption"][
+            0
+        ].__str__()
+        error_message = f"""
+        Sorry, we couldn't find any values for 'Percentage Of Renewable Sources In Total Energy Consumption' datapoint.
+
+        Error was:
+
+        {parse_errors}
+        """
+
+    mo.md(error_message).callout(kind="danger") if error_message else None
+    return error_message, parse_errors
 
 
 @app.cell
